5087ddb0c6c14bcc8eeda27e899e75ca91dcfd5e
e-dupuis <red.life34@gmail.com>
2024-02-18 12:53:03 +0100

    init

---
+++ app/src/main/java/com/example/camerax_mlkit/TextDrawable.kt
@@ -0,0 +1,68 @@
+package com.example.camerax_mlkit
+
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.ColorFilter
+import android.graphics.Paint
+import android.graphics.PixelFormat
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import com.google.mlkit.vision.text.Text.TextBlock
+
+class TextDrawable(private val textBlock: TextBlock) : Drawable() {
+    private val boundingRectPaint = Paint().apply {
+        style = Paint.Style.STROKE
+        color = Color.YELLOW
+        strokeWidth = 5F
+        alpha = 200
+    }
+
+    private val contentRectPaint = Paint().apply {
+        style = Paint.Style.FILL
+        color = Color.YELLOW
+        alpha = 255
+    }
+
+    private val contentTextPaint = Paint().apply {
+        color = Color.DKGRAY
+        alpha = 255
+        textSize = 36F
+    }
+
+    private val contentPadding = 25
+    private var textWidth = contentTextPaint.measureText(textBlock.text).toInt()
+    private val boundingBox = textBlock.boundingBox!!
+
+    override fun draw(canvas: Canvas) {
+        canvas.drawRect(boundingBox, boundingRectPaint)
+        canvas.drawRect(
+            Rect(
+                boundingBox.left,
+                boundingBox.bottom + contentPadding/2,
+                boundingBox.left + textWidth + contentPadding*2,
+                boundingBox.bottom + contentTextPaint.textSize.toInt() + contentPadding),
+            contentRectPaint
+        )
+        canvas.drawText(
+            textBlock.text,
+            (boundingBox.left + contentPadding).toFloat(),
+            (boundingBox.bottom + contentPadding*2).toFloat(),
+            contentTextPaint
+        )
+    }
+
+    override fun setAlpha(alpha: Int) {
+        boundingRectPaint.alpha = alpha
+        contentRectPaint.alpha = alpha
+        contentTextPaint.alpha = alpha
+    }
+
+    override fun setColorFilter(colorFiter: ColorFilter?) {
+        boundingRectPaint.colorFilter = colorFilter
+        contentRectPaint.colorFilter = colorFilter
+        contentTextPaint.colorFilter = colorFilter
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun getOpacity(): Int = PixelFormat.TRANSLUCENT
+}
